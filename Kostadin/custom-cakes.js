94019fb7cb09d748145f051ed664d9c34377bdfa
KostadinHazurov <koce1103@abv.bg>
2026-05-01 19:17:41 +0300

    duzovgeroi

---
+++ Kostadin/custom-cakes.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cakeOrderForm = document.getElementById("cakeOrderForm");
+    const formMessage = document.getElementById("formMessage");
+
+    cakeOrderForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        const requiredFields = cakeOrderForm.querySelectorAll("[required]");
+        
+        let Valid = true;
+        requiredFields.forEach(field => {
+            if (!field.value) {
+                Valid = false;
+                field.classList.add("error");
+            } else {
+                field.classList.remove("error");
+            }
+        });
+
+        if (Valid) {
+            formMessage.classList.remove("hidden", "error");
+            formMessage.classList.add("success");
+            formMessage.textContent = "Благодарим! Ще се свържем с Вас за потвърждение до 24 часа.";
+            cakeOrderForm.reset();
+        } else {
+            formMessage.classList.remove("hidden", "success");
+            formMessage.classList.add("error");
+            formMessage.textContent = "Моля попълнете всички полета!";
+        }
+    });
+});
